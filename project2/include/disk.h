01b1f3ae41aa64e7c26da15f10941ea4c291b291
Sooyoung Baek <2016024857@hanyang.ac.kr>
2020-10-17 12:37:20 +0900

    remove disk.c disk.h

--- project2/include/disk.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DISK_H__
-#define __DISK_H__
-
-#include <stdlib.h>
-#include <stdio.h>
-void print_dsm();
-
-#endif //__DISK_H__
--- project2/src/disk.c
+++
@@ -1,5 +0,0 @@
-#include "disk.h"
-
-void print_dsm(){
-    printf("dsm\n");
-}
